a805638839150c50fa7bc6afffb47302baa4d366
Mithusan <115104826+Mithusan@users.noreply.github.com>
2022-10-12 11:41:02 -0400

    Add files via upload

---
+++ src/assignment/pkg5/sleep/calculator/Assignment5SleepCalculator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment.pkg5.sleep.calculator;
+
+/**
+ *
+ * @author s201060267
+ */
+public class Assignment5SleepCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
